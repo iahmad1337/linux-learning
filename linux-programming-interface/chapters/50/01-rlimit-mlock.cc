c9b8089fdca926731ba6588ed233f9fad73a04bd
Ahmad I <iahmd01@gmail.com>
2025-07-03 02:25:17 +0300

    RLIMIT_MEMLOCK exercise done

--- linux-programming-interface/chapters/50/01-rlimit-mlock.cc
+++ linux-programming-interface/chapters/50/01-rlimit-mlock.cc
@@ -1,14 +1,34 @@
 #include "common.hh"
 
+#include <cassert>
+#include <cstdio>
 #include <cstdlib>
+#include <stdint.h>
 #include <unistd.h>
+#include <sys/resource.h>
+#include <sys/mman.h>
+
+const auto PAGE_SIZE = sysconf(_SC_PAGESIZE);
 
 int main(int argc, char** argv) {
-    file_guard fg{"dummy_file", "abcdefghijklmnopqrstuvwxyz"};
-    auto fd = open("dummy_file", O_WRONLY | O_APPEND);
-    lseek(fd, 0, SEEK_SET);
-    write(fd, "1337", 4);
-    close(fd);
-    // So here we see, that "1337" is actually appended to the end
-    system("cat dummy_file");
+    rlimit rlim;
+    getrlimit(RLIMIT_MEMLOCK, &rlim);
+    printf("RLIMIT_MEMLOCK: cur=%lu max=%lu\n", rlim.rlim_cur, rlim.rlim_max);
+
+    // 1. Set the soft limit to low value
+    rlim.rlim_cur = PAGE_SIZE;
+    setrlimit(RLIMIT_MEMLOCK, &rlim);
+    getrlimit(RLIMIT_MEMLOCK, &rlim);
+    assert(rlim.rlim_cur == PAGE_SIZE);
+
+    char buf[PAGE_SIZE * 10];
+    void* aligned_addr = (void*)(((uintptr_t)buf) & ~(PAGE_SIZE - 1));
+
+    // 2. Try to lock no more than one page - should succeed
+    mlock(aligned_addr, PAGE_SIZE);
+    assert(errno == 0);
+
+    // 3. Try to lock more memory - should fail
+    mlock(aligned_addr, PAGE_SIZE * 2);
+    assert(errno == ENOMEM);
 }
